1407b244b5bb024b7bf332ffb4ac8847d77d0355
Kim Ki Ug <2897robo@naver.com>
2024-11-24 14:45:50 +0900

    Study on November 23th, 2024

--- d_CodingTest/src/Main.java
+++ d_CodingTest/src/Main.java
@@ -1,17 +1,20 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.util.*;
+import java.util.stream.*;
 
 public class Main {
-    public static void modifyArray(int[] arr) {
-        arr[0] = 10; // 배열 내부 값 변경
-        arr = new int[] {20, 30}; // 참조 자체 변경
-    }
-
     public static void main(String[] args) {
-        int[] array = {1, 2, 3};
-        modifyArray(array);
-        System.out.println(array); // 출력: 10 (내부 값 변경 반영)
-        // 그러나 array는 여전히 {1, 2, 3}을 가리킴.
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // 기존 방식
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+
+        // Stream 방식
+        numbers.stream()
+                .filter(num -> num % 2 == 0) // 짝수 필터링
+                .forEach(System.out::println); // 출력
     }
 }
