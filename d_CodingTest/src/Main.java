b82b57e49ce3fa53c98c3901df913b275a579655
Kim Ki Ug <2897robo@naver.com>
2024-11-25 20:46:21 +0900

    Study on November 25th, 2024

--- d_CodingTest/src/Main.java
+++ d_CodingTest/src/Main.java
@@ -3,18 +3,6 @@ import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-
-        // 기존 방식
-        for (int num : numbers) {
-            if (num % 2 == 0) {
-                System.out.println(num);
-            }
-        }
-
-        // Stream 방식
-        numbers.stream()
-                .filter(num -> num % 2 == 0) // 짝수 필터링
-                .forEach(System.out::println); // 출력
+        System.out.println("HI");
     }
 }
