86f7568e41c400c1595a2dc73f94f0edc68f0b60
Kim Ki Ug <2897robo@naver.com>
2024-12-09 20:37:56 +0900

    Study on December 9th, 2024

--- d_CodingTest/src/Main.java
+++ d_CodingTest/src/Main.java
@@ -1,27 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        Set<String> words = new HashSet<>();
+        Person[] p = new Person[n];
+
         for(int i=0; i<n; i++) {
-            words.add(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            p[i] = new Person(Integer.parseInt(st.nextToken()), st.nextToken());
         }
-        br.close();
 
-        List<String> wordsList = new ArrayList<>(words);
-
-        Collections.sort(wordsList, (String a, String b) -> {
-            return (a.length() == b.length()) ? a.compareTo(b) : a.length() - b.length();
+        Arrays.sort(p, new Comparator<Person>() {
+            @Override
+            public int compare(Person s1, Person s2) {
+                return s1.age - s2.age;
+            }
         });
 
-        for(String w : wordsList) {
-            System.out.println(w);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            sb.append(p[i]);
         }
+
+        System.out.print(sb);
+    }
+}
+
+class Person {
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return age + " " + name + "\n";
     }
 }
