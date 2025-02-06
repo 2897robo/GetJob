eaa6cc325558a303a69217594078dcff1370f9dc
2897robo <2897robo@gmail.com>
2025-02-06 17:37:14 +0900

    feat: Java study summary for February 6, 2025
    - Learned about primitive and reference types
    - Covered variable declaration and initialization
    - Studied varargs (variable arguments)
    - Explored Java packages and structure
    - Understood value vs. reference passing
    - Discussed boilerplate code and Lombok usage
    - Reviewed hashCode() and equals() methods
    - Practiced forEach() and stream().forEach() differences
    - Examined handling break in forEach loops

--- d_CodingTest/src/Main.java
+++ d_CodingTest/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String args[]) {
         int a = 1;
         int b = 2;
-        System.out.println("HI");
+        System.out.println("HI".hashCode());
         System.out.println(a+b);
     }
 }
