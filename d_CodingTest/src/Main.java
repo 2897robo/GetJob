93d397da70f2a865bc4b864367005aacbbf9f75f
2897robo <2897robo@gmail.com>
2025-02-07 22:36:24 +0900

    feat: Java study summary for February 7, 2025
    - Learned about Polymorphism, Upcasting, and Downcasting
    - Explored `default` and `static` methods in interfaces
    - Studied the use of `final` keyword in variables, methods, and classes
    - Compared abstract classes and interfaces
    - Understood `this` and `super` keywords
    - Analyzed why static methods are not overridden
    - Reviewed `is-a` and `has-a` relationships
    - Differentiated method overloading (static binding) and overriding (dynamic binding)
    - Summarized key features of Java 14's `record` class

--- d_CodingTest/src/Main.java
+++ d_CodingTest/src/Main.java
@@ -1,8 +1,3 @@
 public class Main {
-    public static void main(String args[]) {
-        int a = 1;
-        int b = 2;
-        System.out.println("HI".hashCode());
-        System.out.println(a+b);
-    }
+
 }
